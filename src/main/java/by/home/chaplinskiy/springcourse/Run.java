5ae7160cc0674735a6f6bcf6cd7c1cd4f3c7addf
Ura <Yurick1771081@gmail.com>
2022-06-21 01:10:14 +0300

    dependency injection (IoC)

--- src/main/java/by/home/chaplinskiy/springcourse/Run.java
+++ src/main/java/by/home/chaplinskiy/springcourse/Run.java
@@ -8,7 +8,19 @@ public class Run {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         TestBean testBean = context.getBean("testBean", TestBean.class);
         System.out.println(testBean.getName());
-        context.close();
+
+        Music musicTecho = context.getBean("musicTechoBean", Music.class);
+        MusicPlayer musicPlayer1 = new MusicPlayer(musicTecho);
+        musicPlayer1.playMusic();
+
+        Music musicPop = context.getBean("musicPopBean", Music.class);
+        MusicPlayer musicPlayer2 = new MusicPlayer(musicPop);
+        musicPlayer2.playMusic();
+
+        Music music = context.getBean("musicBean", Music.class);
+        MusicPlayer musicPlayer3 = new MusicPlayer(music);
+        musicPlayer3.playMusic();
+
     }
 
 }
