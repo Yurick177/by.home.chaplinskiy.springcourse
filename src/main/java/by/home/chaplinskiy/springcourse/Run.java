44b9aba078c57509020cceae16ba2ac9e1c9b02e
Ura <Yurick1771081@gmail.com>
2022-06-20 23:52:34 +0300

    create class "Run" in it create context and did method "getBean"

---
+++ src/main/java/by/home/chaplinskiy/springcourse/Run.java
@@ -0,0 +1,14 @@
+package by.home.chaplinskiy.springcourse;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Run {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        TestBean testBean = context.getBean("testBean", TestBean.class);
+        System.out.println(testBean.getName());
+        context.close();
+    }
+
+}
