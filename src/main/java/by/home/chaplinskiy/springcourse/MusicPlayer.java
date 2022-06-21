3d6198741b8a33117c18eed6d5bcf985d32d6ae1
Ura <Yurick1771081@gmail.com>
2022-06-22 01:53:50 +0300

    changed class "MusicPlayer" so that it plays not one genre, but a list

--- src/main/java/by/home/chaplinskiy/springcourse/MusicPlayer.java
+++ src/main/java/by/home/chaplinskiy/springcourse/MusicPlayer.java
@@ -1,16 +1,15 @@
 package by.home.chaplinskiy.springcourse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
 
-    private  Music music;
+    private List<Music> musicList = new ArrayList<>();
     private String name;
     private int volume;
 
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
-
-    public MusicPlayer(){
+    public MusicPlayer() {
 
     }
 
@@ -30,12 +29,14 @@ public class MusicPlayer {
         this.volume = volume;
     }
 
-    public void setMusic(Music music){
-       this.music = music;
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
-    public void playMusic(){
-        System.out.println("Playing: " + music.getSong());
+    public void playMusicList() {
+        for (Music music : musicList) {
+            System.out.println("Playing: " + music.getSong());
+        }
     }
 
 }
--- src/main/java/by/home/chaplinskiy/springcourse/Run.java
+++ src/main/java/by/home/chaplinskiy/springcourse/Run.java
@@ -9,35 +9,10 @@ public class Run {
         TestBean testBean = context.getBean("testBean", TestBean.class);
         System.out.println(testBean.getName());
 
-        Music musicTechno = context.getBean("musicTechnoBean", Music.class);
-        MusicPlayer musicPlayer1 = new MusicPlayer(musicTechno);
-        musicPlayer1.playMusic();
-
-        Music musicPop = context.getBean("musicPopBean", Music.class);
-        MusicPlayer musicPlayer2 = new MusicPlayer(musicPop);
-        musicPlayer2.playMusic();
-
-        Music music = context.getBean("musicBean", Music.class);
-        MusicPlayer musicPlayer3 = new MusicPlayer(music);
-        musicPlayer3.playMusic();
-
-        MusicPlayer musicPlayer4 = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer4.playMusic();
-
-        MusicPlayer musicPlayer5 = context.getBean("musicPlayerTechno", MusicPlayer.class);
-        musicPlayer5.playMusic();
-        System.out.println();
-
-        MusicPlayer musicPlayer6 = context.getBean("musicPlayerPop", MusicPlayer.class);
-        musicPlayer6.playMusic();
-        System.out.println(musicPlayer6.getVolume());
-        System.out.println(musicPlayer6.getName());
-        System.out.println();
-
-        MusicPlayer musicPlayer7 = context.getBean("musicPlayerRock", MusicPlayer.class);
-        musicPlayer7.playMusic();
-        System.out.println(musicPlayer7.getVolume());
-        System.out.println(musicPlayer7.getName());
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        musicPlayer.playMusicList();
+        System.out.println(musicPlayer.getVolume());
+        System.out.println(musicPlayer.getName());
     }
 
 }
