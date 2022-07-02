784dbcc9340c80f1a59315fa94133175228e5cdc
Ura <Yurick1771081@gmail.com>
2022-07-03 01:26:21 +0300

    delete context "ClassPathXmlApplicationContext" and add context "AnnotationConfigApplicationContext"

--- src/main/java/by/home/chaplinskiy/springcourse/Run.java
+++ src/main/java/by/home/chaplinskiy/springcourse/Run.java
@@ -1,14 +1,16 @@
 package by.home.chaplinskiy.springcourse;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Run {
 
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
         MusicPlayer technoMusicPlayer = context.getBean(MusicPlayer.class);
         MusicPlayer technoMusicPlayer2 = context.getBean(MusicPlayer.class);
-        System.out.println(technoMusicPlayer2==technoMusicPlayer);
+        System.out.println(technoMusicPlayer2 == technoMusicPlayer);
         System.out.println(technoMusicPlayer.playMusic());
         System.out.println(technoMusicPlayer.getName());
         System.out.println(technoMusicPlayer.getVolume());
