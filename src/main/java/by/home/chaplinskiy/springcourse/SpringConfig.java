6014761acba9bc60410065293b6ee5bcafac3e9e
Ura <Yurick1771081@gmail.com>
2022-07-03 01:21:38 +0300

    add class "SpringConfig" annotated "@ComponentScan,PropertySource"

--- src/main/java/by/home/chaplinskiy/springcourse/SpringConfig.java
+++ src/main/java/by/home/chaplinskiy/springcourse/SpringConfig.java
@@ -1,8 +1,12 @@
 package by.home.chaplinskiy.springcourse;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@ComponentScan("by.home.chaplinskiy.springcourse")
+@PropertySource("classpath:musicPlayer.properties")
 public class SpringConfig {
 
 }
